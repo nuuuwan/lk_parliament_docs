6e6e339b3900f7fb0566cfef8a3fd6baeec13afb
nuuuwan <nuuuwan@gmail.com>
2025-08-16 11:06:25 +0530

    Added workflow/scrape

---
+++ workflows/scrape.py
@@ -0,0 +1,40 @@
+import sys
+import time
+
+from utils import Log
+
+from lpd import ActsBillsPage
+
+log = Log("scrape")
+DEFAULT_MAX_DT = 1
+MAX_YEAR = 2025
+MIN_YEAR = 1800
+
+
+def scrape_year(year):
+    log.debug("-" * 32)
+    log.info(f"Running scrape for {year=}")
+    page = ActsBillsPage("acts", str(year))
+    page.scrape()
+    log.debug("-" * 32)
+
+
+def scrape(max_dt):
+    year = MAX_YEAR
+    t_start = time.time()
+    while True:
+        dt = time.time() - t_start
+        if dt > max_dt:
+            log.info(f"Stopping. 🛑 {dt:.1f}s > {max_dt}s.")
+            sys.exit(0)
+
+        scrape_year(year)
+
+        year -= 1
+        if year < MIN_YEAR:
+            log.info(f"Stopping. 🛑 {year} < {MIN_YEAR}")
+            sys.exit(0)
+
+
+if __name__ == "__main__":
+    scrape(max_dt=sys.argv[1] if len(sys.argv) > 1 else DEFAULT_MAX_DT)
