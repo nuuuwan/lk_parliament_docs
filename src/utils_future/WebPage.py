3e69e4e829a3022f664de7328127105968acc244
nuuuwan <nuuuwan@gmail.com>
2025-08-16 12:02:07 +0530

    Use Chrome

--- src/utils_future/WebPage.py
+++ src/utils_future/WebPage.py
@@ -1,8 +1,8 @@
 import time
 
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from utils import Log
 
 log = Log("WebPage")
@@ -16,10 +16,12 @@ class WebPage:
     def open(self):
         assert self.driver is None
         options = Options()
-        options.add_argument("--headless")
+        options.add_argument("--headless=new")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
 
-        self.driver = webdriver.Firefox(
-            options=options, service=Service(log_path="geckodriver.log")
+        self.driver = webdriver.Chrome(
+            options=options, service=Service(log_path="chromedriver.log")
         )
         self.driver.set_window_size(3200, 3200)
         self.driver.get(self.url)
@@ -27,7 +29,8 @@ class WebPage:
         return self.driver
 
     def quit(self):
-        self.driver.quit()
+        if self.driver:
+            self.driver.quit()
 
     def sleep(self, t):
         log.debug(f"😴 {t}s")
