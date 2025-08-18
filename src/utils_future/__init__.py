56095ffde2a6dd292fbe22188cf0b9fc508be5dd
nuuuwan <nuuuwan@gmail.com>
2025-08-18 11:58:20 +0530

    Import markdown

--- src/utils_future/__init__.py
+++ src/utils_future/__init__.py
@@ -1,5 +1,6 @@
 # utils_future (auto generate by build_inits.py)
 # flake8: noqa: F408
 
+from utils_future.Markdown import Markdown
 from utils_future.Parse import Parse
 from utils_future.WebPage import WebPage
