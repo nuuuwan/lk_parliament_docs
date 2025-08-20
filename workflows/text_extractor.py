4fa810842d983ddb9396f4f6b1a61005405e28cf
nuuuwan <nuuuwan@gmail.com>
2025-08-20 12:21:29 +0530

    Use TimedPipeline

--- workflows/text_extractor.py
+++ workflows/text_extractor.py
@@ -1,21 +1,10 @@
-import sys
-import time
-
 from utils import Log
 
-from lk_acts import Act, ActExt
+from lk_acts import Act
+from utils_future import TimedPipeline
 from workflows.metadata_scraper import DEFAULT_MAX_DT, get_options
 
-log = Log("scrape")
-
-
-def text_extractor_for_act(act):
-    act.extract_txt()
-
-    try:
-        ActExt.from_pdf(act.pdf_path).build(act.act_id)
-    except Exception as e:
-        log.error(f"Error processing {act.act_id}: {e}")
+log = Log("text_extractor")
 
 
 def text_extractor(max_dt, decade):
@@ -23,16 +12,7 @@ def text_extractor(max_dt, decade):
     log.debug(f"{decade=}")
     act_list = Act.list_from_decade(decade)
 
-    t_start = time.time()
-    for act in act_list:
-        dt = time.time() - t_start
-        if dt > max_dt:
-            log.info(f"Stopping. 🛑 {dt:.0f}s > {max_dt}s.")
-            sys.exit(0)
-
-        text_extractor_for_act(act)
-
-    log.info("Stopping. 🛑 ALL acts complete.")
+    TimedPipeline(max_dt, lambda act: act.extract_text(act), act_list).run()
 
 
 if __name__ == "__main__":
