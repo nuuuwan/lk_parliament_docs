a88bef69245e27f9f1d2f40a6497ce0795fdbc6b
nuuuwan <nuuuwan@gmail.com>
2025-08-21 12:45:05 +0530

    enable tests

--- tests/test_pdf_file.py
+++ tests/test_pdf_file.py
@@ -29,30 +29,30 @@ class TestCase(unittest.TestCase):
             },
         )
 
-    # def test_get_image_text(self):
-    #     image_text = TEST_PDF_FILE.get_image_text()
-    #     self.assertGreater(len(image_text), 1_000)
+    def test_get_image_text(self):
+        image_text = TEST_PDF_FILE.get_image_text()
+        self.assertGreater(len(image_text), 1_000)
 
-    # def test_compress(self):
-    #     compressed_pdf_file = TEST_PDF_FILE.compress()
-    #     self.assertLessEqual(compressed_pdf_file.size, TEST_PDF_FILE.size)
+    def test_compress(self):
+        compressed_pdf_file = TEST_PDF_FILE.compress()
+        self.assertLessEqual(compressed_pdf_file.size, TEST_PDF_FILE.size)
 
-    # def test_get_text_content_with_compress(self):
-    #     compressed_pdf_file = TEST_PDF_FILE.compress()
+    def test_get_text_content_with_compress(self):
+        compressed_pdf_file = TEST_PDF_FILE.compress()
 
-    #     image_text_from_compressed = compressed_pdf_file.get_image_text()
-    #     self.assertGreater(len(image_text_from_compressed), 1_000)
+        image_text_from_compressed = compressed_pdf_file.get_image_text()
+        self.assertGreater(len(image_text_from_compressed), 1_000)
 
-    #     image_text_from_uncompressed = TEST_PDF_FILE.get_image_text()
+        image_text_from_uncompressed = TEST_PDF_FILE.get_image_text()
 
-    #     File(os.path.join("tests", "data", "image_compressed.txt")).write(
-    #         image_text_from_compressed
-    #     )
-    #     File(os.path.join("tests", "data", "image_uncompressed.txt")).write(
-    #         image_text_from_uncompressed
-    #     )
+        File(os.path.join("tests", "data", "image_compressed.txt")).write(
+            image_text_from_compressed
+        )
+        File(os.path.join("tests", "data", "image_uncompressed.txt")).write(
+            image_text_from_uncompressed
+        )
 
-    #     ratio = len(image_text_from_compressed) / len(
-    #         image_text_from_uncompressed
-    #     )
-    #     self.assertGreater(ratio, 0.9)
+        ratio = len(image_text_from_compressed) / len(
+            image_text_from_uncompressed
+        )
+        self.assertGreater(ratio, 0.9)
