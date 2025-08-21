9b46f29ea105ea3ed6a3bd50cb7cb0670c5e930a
nuuuwan <nuuuwan@gmail.com>
2025-08-21 18:36:57 +0530

    Added more details

--- workflows/oneoff/clean_temp.py
+++ workflows/oneoff/clean_temp.py
@@ -7,22 +7,28 @@ from lk_acts.core.act import Act
 log = Log("clean_temp")
 
 
+def delete_files_if_exists(act, file_path_list):
+    for delete_path in file_path_list:
+        if os.path.exists(delete_path):
+            os.remove(delete_path)
+            log.debug(f"[{act}] ❌ Deleted {delete_path}")
+
+
 def clean_temp_for_act(act):
-    print(f"[{act}] Cleaning legacy files.", end="\r")
     if os.path.exists(act.blocks_path):
         blocks = JSONFile(act.blocks_path).read()
         block_text = "\n\n".join([block["text"] for block in blocks])
         if len(block_text) < Act.MIN_BLOCK_TEXT_CHARS:
-            for delete_path in [act.blocks_path, act.text_path]:
-                if os.path.exists(delete_path):
-                    os.remove(delete_path)
-                    log.debug(f"[{act}] ❌ Deleted {delete_path}")
+            delete_files_if_exists(act, [act.blocks_path, act.text_path])
+    else:
+        delete_files_if_exists(act, [act.text_path])
 
 
 def clean_temp():
     act_list = Act.list_all()
     for act in act_list:
         clean_temp_for_act(act)
+    log.info(f"clean_temp Complete.")
 
 
 if __name__ == "__main__":
