8d064cafbcc2c4e8c844d7f333e3b13172045b43
nuuuwan <nuuuwan@gmail.com>
2025-08-21 12:28:01 +0530

    Added extract_blocks

--- src/lk_acts/core/act/ActExtractText.py
+++ src/lk_acts/core/act/ActExtractText.py
@@ -1,7 +1,7 @@
 import os
 from functools import cached_property
 
-from utils import File, Log
+from utils import File, JSONFile, Log
 
 from utils_future import PDFFile
 
@@ -26,3 +26,19 @@ class ActExtractText:
         except Exception as e:
             log.error(f"[{self}] {e}")
             return None
+
+    @cached_property
+    def blocks_json_path(self):
+        return os.path.join(self.dir_act_data, "blocks.json")
+
+    def extract_blocks(self):
+        if not os.path.exists(self.pdf_path):
+            return None
+        if os.path.exists(self.blocks_json_path):
+            return self.blocks_json_path
+
+        blocks_info_list = PDFFile(self.pdf_path).get_blocks_info_list()
+        n_blocks = len(blocks_info_list)
+        JSONFile(self.blocks_json_path).write(blocks_info_list)
+        log.info(f"Wrote {self.blocks_json_path} ({n_blocks:,} blocks)")
+        return self.blocks_json_path
