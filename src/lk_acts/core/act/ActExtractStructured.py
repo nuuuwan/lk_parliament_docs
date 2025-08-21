f3fa525df8c8855734d771dc6e3563185772e66a
nuuuwan <nuuuwan@gmail.com>
2025-08-21 17:07:36 +0530

    ActExtractStructured STUB

---
+++ src/lk_acts/core/act/ActExtractStructured.py
@@ -0,0 +1,6 @@
+class ActExtractStructured:
+    def structured_json_path(self):
+        return os.path.join(self.dir_act_data, "structured.json")
+
+    def extract_structured(self):
+        pass
