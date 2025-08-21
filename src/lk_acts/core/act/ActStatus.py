17c4bca43a4aae6b28588048631c9eea8c6b3da7
nuuuwan <nuuuwan@gmail.com>
2025-08-21 16:21:21 +0530

    FIX src/lk_acts/core/act/ActStatus.py:39:5: CFQ001 Function get_status_summary has length 43 that exceeds max allowed length 40

--- src/lk_acts/core/act/ActStatus.py
+++ src/lk_acts/core/act/ActStatus.py
@@ -35,48 +35,35 @@ class ActStatus:
             has_act_json=self.has_act_json,
         )
 
+    @staticmethod
+    def __get_totals_row__(d_list):
+        k_list = list(d_list[0].keys())
+        totals_d = dict(decade="Total")
+        for k in k_list:
+            if k != "decade":
+                totals_d[k] = sum(d[k] for d in d_list)
+        return totals_d
+
     @classmethod
     def get_status_summary(cls):
         decade_to_list = cls.decade_to_list()
-
         d_list = []
         for decade, act_list in decade_to_list.items():
-            n_metadata = sum(
-                [1 for act in act_list if act.status["has_metadata"]]
-            )
-            n_pdf = sum([1 for act in act_list if act.status["has_pdf"]])
-            n_blocks = sum(
-                [1 for act in act_list if act.status["has_blocks"]]
-            )
-            n_txt = sum([1 for act in act_list if act.status["has_txt"]])
-            n_act_json = sum(
-                [1 for act in act_list if act.status["has_act_json"]]
-            )
-
             d = dict(
                 decade=decade,
-                n_metadata=n_metadata,
-                n_pdf=n_pdf,
-                n_blocks=n_blocks,
-                n_txt=n_txt,
-                n_act_json=n_act_json,
+                n_metadata=sum(
+                    [1 for act in act_list if act.status["has_metadata"]]
+                ),
+                n_pdf=sum([1 for act in act_list if act.status["has_pdf"]]),
+                n_blocks=sum(
+                    [1 for act in act_list if act.status["has_blocks"]]
+                ),
+                n_txt=sum([1 for act in act_list if act.status["has_txt"]]),
+                n_act_json=sum(
+                    [1 for act in act_list if act.status["has_act_json"]]
+                ),
             )
             d_list.append(d)
 
-        total_n_metadata = sum(d["n_metadata"] for d in d_list)
-        total_n_pdf = sum(d["n_pdf"] for d in d_list)
-        total_n_blocks = sum(d["n_blocks"] for d in d_list)
-        total_n_txt = sum(d["n_txt"] for d in d_list)
-        total_n_act_json = sum(d["n_act_json"] for d in d_list)
-
-        totals_d = dict(
-            decade="Total",
-            n_metadata=total_n_metadata,
-            n_pdf=total_n_pdf,
-            n_blocks=total_n_blocks,
-            n_txt=total_n_txt,
-            n_act_json=total_n_act_json,
-        )
-        d_list.append(totals_d)
-
+        d_list.append(cls.__get_totals_row__(d_list))
         return d_list
