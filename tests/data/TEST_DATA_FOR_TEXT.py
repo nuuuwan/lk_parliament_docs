5c5a2b10e1fa97cb9b8c665b963477d2f8d70b0a
nuuuwan <nuuuwan@gmail.com>
2025-08-23 11:29:09 +0530

    test_text

---
+++ tests/data/TEST_DATA_FOR_TEXT.py
@@ -0,0 +1,29 @@
+# flake8: noqa
+TEST_DATA_FOR_TEXT = [
+    [
+        "2025-013",
+        133,
+        [
+            "3$5/,$0(17\x03\x032)\x03\x037+(\x03\x03'(02&5$7,&\x0362&,$/,67\x03\x035(38%/,&\x03\x032)\x0365,\x03\x03/$1.$",
+            "35,17('\x03$7\x037+(\x03'(3$570(17\x032)\x03*29(510(17\x0335,17,1*\x0f\x0365,\x03/$1.$BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
+            "72\x03%(\x03385&+$6('\x03$7\x037+(\x03*29(510(17\x0338%/,&$7,216\x03%85($8\x0f\x03&2/20%2\x03\x18",
+            "Price : Rs. 40.00\x03Postage : Rs. 150.00",
+            "This Act can be downloaded from ZZZ\x11GRFXPHQWV\x11JRY\x11ON",
+            "BILLS OF EXCHANGE (AMENDMENT)",
+            "ACT, No. 13 OF 2025",
+            ">&HUWL\x03HG\x03RQ\x03\x14\x18WK\x03RI\x03$XJXVW\x0f\x03\x15\x13\x15\x18@",
+            "Printed on the Order of Government",
+            "3XEOLVKHG\x03DV\x03D\x036XSSOHPHQW\x03WR\x033DUW\x03,,\x03RI\x03WKH\x03*D]HWWH\x03RI\x03WKH\x03'HPRFUDWLF\x036RFLDOLVW\x035HSXEOLF\x03RI\x036UL\x03/DQND\x03RI\x03$XJXVW\x03\x14\x18\x0f\x03\x15\x13\x15\x18",
+            "1Bills of Exchange (Amendment) Act, No. 13 of 2025",
+            "[Certified on 15th of August, 2025]",
+            "L.D.-O 10/2024",
+            "An Act to amend the Bills of Exchange Ordinance(Chapter 82)",
+            "BE it enacted by the Parliament of the Democratic Socialist Republic of Sri Lanka as follows: -",
+            "1. This Act may be cited as the Bills of Exchange(Amendment) Act, No. 13 of 2025.",
+            "2. The Bills of Exchange Ordinance (Chapter 82)(hereinafter referred to as the principal enactment) is hereby amended by the substitution for the word Ceylon, wherever that word occurs in the principal enactment, of the words Sri Lanka.",
+            "3. Section 2 of the principal enactment is hereby amended as follows: -",
+            "(1) by the substitution for the definition of theexpression banker, of the following definition: -",
+            "banker means a licensed commercial bank or a licensed specialised bank or a branch of a foreign bank incorporated outside Sri Lanka, which carries on the business of banking within the meaning of the Banking Act, No.30 of 1988;; and",
+        ],
+    ]
+]
--- tests/test_pdf_file.py
+++ tests/test_pdf_file.py
@@ -2,17 +2,59 @@ import os
 import unittest
 
 from tests.data.TEST_DATA_FOR_OCR import TEST_DATA_FOR_OCR
+from tests.data.TEST_DATA_FOR_TEXT import TEST_DATA_FOR_TEXT
 from utils_future import PDFFile
 
 TEST_PDF_FILE = PDFFile(os.path.join("tests", "data", "image-2790.pdf"))
 
 
 class TestCase(unittest.TestCase):
-
+    @unittest.skip("Slow")
     def test_compress(self):
         compressed_pdf_file = TEST_PDF_FILE.compress()
         self.assertLessEqual(compressed_pdf_file.size, TEST_PDF_FILE.size)
 
+    def test_text(self):  # noqa: CFQ001
+        N_HEAD = 20
+        a_list = []
+        for (
+            file_id,
+            e_n_block_info_list,
+            e_text_list_head,
+        ) in TEST_DATA_FOR_TEXT:
+
+            pdf_file = PDFFile(
+                os.path.join("tests", "data", f"{file_id}-en.pdf")
+            )
+
+            block_info_list = pdf_file.get_block_info_list()
+            a_n_block_info_list = len(block_info_list)
+
+            a_text_list_head = [d["text"] for d in block_info_list[:N_HEAD]]
+
+            a = [
+                file_id,
+                a_n_block_info_list,
+                a_text_list_head,
+            ]
+            a_list.append(a)
+        print(a_list)
+
+        for e, a in zip(TEST_DATA_FOR_TEXT, a_list):
+            [
+                file_id,
+                e_n_block_info_list,
+                e_text_list_head,
+            ] = e
+            [
+                _,
+                a_n_block_info_list,
+                a_text_list_head,
+            ] = a
+            self.assertEqual(a_n_block_info_list, e_n_block_info_list)
+            self.assertEqual(a_text_list_head, e_text_list_head)
+
+    @unittest.skip("Slow")
     def test_ocr(self):  # noqa: CFQ001
         N_HEAD = 20
         a_list = []
@@ -20,7 +62,7 @@ class TestCase(unittest.TestCase):
             file_id,
             e_n_block_info_list,
             e_n_ocr_block_info_list,
-            e_text_list_head,
+            e_ocr_text_list_head,
         ) in TEST_DATA_FOR_OCR:
 
             pdf_file = PDFFile(
@@ -32,7 +74,7 @@ class TestCase(unittest.TestCase):
 
             ocr_block_info_list = pdf_file.get_ocr_block_info_list()
             a_n_ocr_block_info_list = len(ocr_block_info_list)
-            a_text_list_head = [
+            a_ocr_text_list_head = [
                 d["text"] for d in ocr_block_info_list[:N_HEAD]
             ]
 
@@ -40,7 +82,7 @@ class TestCase(unittest.TestCase):
                 file_id,
                 a_n_block_info_list,
                 a_n_ocr_block_info_list,
-                a_text_list_head,
+                a_ocr_text_list_head,
             ]
             a_list.append(a)
 
@@ -49,14 +91,14 @@ class TestCase(unittest.TestCase):
                 file_id,
                 e_n_block_info_list,
                 e_n_ocr_block_info_list,
-                e_text_list_head,
+                e_ocr_text_list_head,
             ] = e
             [
                 _,
                 a_n_block_info_list,
                 a_n_ocr_block_info_list,
-                a_text_list_head,
+                a_ocr_text_list_head,
             ] = a
             self.assertEqual(a_n_block_info_list, e_n_block_info_list)
             self.assertEqual(a_n_ocr_block_info_list, e_n_ocr_block_info_list)
-            self.assertEqual(a_text_list_head, e_text_list_head)
+            self.assertEqual(a_ocr_text_list_head, e_ocr_text_list_head)
