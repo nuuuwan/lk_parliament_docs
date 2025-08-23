1d9a7587a4d2423354446d881b86f14f65931ec0
nuuuwan <nuuuwan@gmail.com>
2025-08-23 17:07:18 +0530

    Added test_first_act

--- tests/test_act.py
+++ tests/test_act.py
@@ -7,3 +7,17 @@ class TestCase(unittest.TestCase):
     def test_list_all(self):
         doc_list = Act.list_all()
         self.assertGreaterEqual(len(doc_list), 0)
+
+    def test_first_act(self):
+        first_act = Act.list_all()[0]
+        self.assertEqual(
+            first_act.to_dict(),
+            {
+                "num": "14/2025",
+                "date": "2025-08-18",
+                "description": "Sri Lanka Electricity (Amendment)",
+                "url_pdf_en": "https://www.parliament.lk/uploads/acts/gbills/english/6385.pdf",
+                "act_id": "2025-014",
+                "act_type": "Amendment",
+            },
+        )
